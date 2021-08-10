bf1b9671dc37732fd982756931aef07b8256f4be
Mark.Kovalyov <mark.kovalyov78@gmail.com>
2021-08-10 11:26:27 +0300

    Graal changes

--- image-indexer-build.sh
+++ image-indexer-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-mkdir -p bin
+mkdir -p bin/linux
+mkdir -p bin/win
+
 mvn package -P default
 
 cp -f target/mtn-img-indexer-1.0-SNAPSHOT.jar ./bin/mtn-img-indexer.jar
 cp -f src/main/resources/mtn-img-indexer.sh ./bin
 cp -f src/main/resources/mtn-img-indexer.cmd ./bin
+
+cp -f target/mtn-img-indexer ./bin/linux
 
